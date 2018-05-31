63a89280bfd39575d52a04a172e59bc97f210f6a
retgits <lstigter@gmail.com>
2018-05-31 09:36:56 -0700

    Added null activity and removed HTTP trigger (merged to flogo-contrib)

---
+++ activity/null/README.md
@@ -0,0 +1,29 @@
+# Null
+
+An activity that does nothing... useful if you want to branch out after the trigger
+
+
+## Installation
+
+```bash
+flogo install github.com/retgits/flogo-components/activity/null
+```
+Link for flogo web:
+```
+https://github.com/retgits/flogo-components/activity/null
+```
+
+## Schema
+Inputs and Outputs:
+
+```json
+{
+    "inputs": [],
+    "outputs": []
+}
+```
+## Inputs
+_There are no inputs_
+
+## Ouputs
+_There are no outputs_
---
+++ activity/null/activity.go
@@ -0,0 +1,31 @@
+// Package null implements an activity that does nothing. This would also be a good template to start actvity building...
+package null
+
+import (
+	"github.com/TIBCOSoftware/flogo-lib/core/activity"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+// log is the default package logger
+var log = logger.GetLogger("activity-null")
+
+// MyActivity is a stub for your Activity implementation
+type MyActivity struct {
+	metadata *activity.Metadata
+}
+
+// NewActivity creates a new activity
+func NewActivity(metadata *activity.Metadata) activity.Activity {
+	return &MyActivity{metadata: metadata}
+}
+
+// Metadata implements activity.Activity.Metadata
+func (a *MyActivity) Metadata() *activity.Metadata {
+	return a.metadata
+}
+
+// Eval implements activity.Activity.Eval
+func (a *MyActivity) Eval(context activity.Context) (done bool, err error) {
+	log.Debug("Executing the null activity...")
+	return true, nil
+}
